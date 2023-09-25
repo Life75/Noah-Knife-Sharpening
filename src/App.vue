734516f2222659a76abc5d4e34a2c13e213e6149
Life75 <austynwashington@yahoo.com>
2023-09-25 09:57:40 -0400

    working on website background image

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,56 @@
 <script setup lang="ts">
-import Main from './components/Main.vue';
-import NavMenu from './components/NavMenu.vue';
+//import Main from './components/Main.vue';
+import NavMenu from "./components/NavMenu.vue";
+
+//function isMobile() {
+//   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+//     return true
+//   } else {
+//     return false
+//   }
+//  }
 </script>
 
 <template>
-  <div id="page-container" class="flex-col" style="margin-right:5%; margin-left: 5%;  flex-direction:column; gap: 4em; height: 100vh; margin: 0 5%; font-family: Cormorant;">
-    <NavMenu></NavMenu>
+  <div id="backdrop" class="absolute brightness-50"></div>
+  <div
+    id="page-container"
+    class="flex-col"
+    style="
+      margin-right: 5%;
+      margin-left: 5%;
+      flex-direction: column;
+      gap: 4em;
+      height: 100vh;
+      margin: 0 5%;
+      font-family: Cormorant;
+    "
+  >
 
-    <Main class="text-center"></Main>
+    <NavMenu></NavMenu>
   </div>
+
+ 
 </template>
 
-<style >
+<style>
+#backdrop {
+  /* Background pattern from Toptal Subtle Patterns */
+  background-image: url('assets/images/scissors.jpg');
+  height: 75%;
+  width: 100%;
+  z-index: -20;
+  background-repeat: no-repeat;
+  background-position: center; 
+  
+}
 #page-container {
   position: relative;
   min-height: 100vh;
 }
 
 #content-wrap {
-  padding-bottom: 2.5rem;    /* Footer height */
+  padding-bottom: 2.5rem; /* Footer height */
 }
 
 #footer {
@@ -26,12 +58,12 @@ import NavMenu from './components/NavMenu.vue';
   bottom: 0;
   width: 40%;
   left: 80%;
-  align-self:flex-end;
-  height: 2.5rem;            /* Footer height */
+  align-self: flex-end;
+  height: 2.5rem; /* Footer height */
 }
 
 @font-face {
   font-family: Cormorant;
-  src: url('assets/fonts/Cormorant-VariableFont_wght.ttf')
+  src: url("assets/fonts/Cormorant-VariableFont_wght.ttf");
 }
 </style>
--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="NavMenu">
         <div class="flex align-middle pt-4 justify-center ">
-            <p class="text-center  text-3xl font-semibold m-auto">Noah's Knife Sharpening</p>
+            <p class="text-center pl-14 text-3xl font-semibold m-auto">Noah's Knife Sharpening</p>
             <div class="pt-1">
                 <svg class="cursor-pointer" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 48 48">
                     <path fill="#039be5" d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"></path><path fill="#fff" d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"></path>
@@ -14,7 +14,7 @@
         <div class="flex justify-center align-middle py-4">
             <el-avatar class=" " :size="115" :src="circleUrl"/>
         </div>
-        <div class="flex justify-center align-middle space-x-40 text-center text-xl cursor-default " style="margin-right: 110px">
+        <div class="flex justify-center align-middle space-x-16 text-center text-xl cursor-default " style="margin-right: 110px">
 
                 <p className=" relative group">
                     <div class="cursor-pointer">Make An Appointment</div>
@@ -28,6 +28,11 @@
                 
                 <p className=" relative group">
                     <div class="cursor-pointer">Call Now</div>
+                <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-white transition-all group-hover:w-full"></span>
+                </p>
+
+                <p className=" relative group">
+                    <div class="cursor-pointer">About Us</div>
                 <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-white transition-all group-hover:w-full"></span>
                 </p>
         </div>
