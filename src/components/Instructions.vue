fb7abe9fa3155204089364d9e99a243fd7b9f3f4
Life75 <austynwashington@yahoo.com>
2023-11-07 18:31:21 -0500

    gabagoo

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="instructions" class="rounded shadow bg-[#3b3837]">
-    <div class="py-5">
-      <p class="text-center text-4xl font-extrabold">By Appointment only</p>
+  <div id="instructions" class="py-5 rounded shadow bg-[#353332]">
+    <div class="">
+      <p class="text-center text-4xl font-extrabold">By Appointment Only</p>
     </div>
     <div class="m-7">
       <p class="text-2xl text-center">
@@ -9,27 +9,27 @@
         tools like kitchen cutlery, scissors, garden tools, hatchets and axes, swords,
         pocket and hunting knives, lawnmower blades, and more.
       </p>
-      <p class="text-center text-3xl py-10 font-extrabold">Options for services</p>
+      <p class="text-center text-3xl py-10 font-bold">Options for services</p>
       <span class="md:flex justify-center">
-        <div class="max-w-md py-4 px-8 bg-gray-50 shadow-lg rounded-lg mb-20">
+        <div class="max-w-md py-4 px-8 bg-gray-100 shadow-lg rounded-lg mb-20">
           
           <div>
-            <h2 class="text-gray-800 text-4xl font-semibold">Mobile</h2>
-            <p class="mt-2 text-xl text-gray-600">
+            <h2 class="text-gray-800 text-4xl font-semibold text-center">Mobile</h2>
+            <p class="mt-2 text-2xl text-gray-600">
                 For mobile, I will come out to you with my equipment and sharpen your knives while you're in the comfort of your own home. 
             </p>
-            <p class="mt-2 text-xl text-gray-600">
+            <p class="mt-2 text-2xl text-gray-600">
                 Mobile Service For Knives: $15 fee. $3 fee per mile after 10 mi. Call or text the number provided for details and setting up an appointment.
 
-            </p>
+            </p>,
           </div>
         
         </div>
         <div class="px-2"></div>
-        <div class="max-w-md py-4 px-8 bg-gray-50  shadow-lg rounded-lg mb-20">
+        <div class="max-w-md py-4 px-8 bg-gray-100  shadow-lg rounded-lg mb-20">
           <div>
-            <h2 class="text-gray-800 text-4xl font-semibold">Drop Off</h2>
-            <p class="mt-2 text-xl text-gray-600">
+            <h2 class="text-gray-800 text-4xl font-semibold text-center">Drop Off</h2>
+            <p class="mt-2 text-xl text-gray-600 text-center ">
                 For drop off, you can come and drop off your knives at my residence.
             </p>
           </div>
@@ -37,8 +37,9 @@
         </div>
       </span>
 
-      <div class="text-center">
-        Prices: $5 minimum per blade $1 per inch after 5 inches $1.5 per inch for serrated //fix
+      <div class="text-center text-2xl">
+      <p class="text-4xl pb-2 font-semibold">Prices</p>
+        $5 minimum per blade $1 per inch after 5 inches $1.5 per inch for serrated
       </div>
     </div>
     <!--Before starting on this sketch out and make similar to the "how does it work" component in this website as a reference: https://www.realtimecolors.com/?colors=e9c46a-264653-e76f51-2a9d8f-e9c46a&fonts=Poppins-Poppins-->
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="main" class="pt-10">
-    <P class="text-5xl text-center pt-20">Instructions</P>
     <Instructions class="mt-10 "/>
     <Testimonals class="pt-10"/>
 
@@ -10,6 +9,7 @@
 <script setup lang="ts">
 import Testimonals from './Testimonals.vue';
 import Instructions from './Instructions.vue';
+//    <P class="text-5xl text-center pt-20">Instructions</P>
 
 </script>
 
